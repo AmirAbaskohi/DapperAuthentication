160797ccd921d0b4298a0ff6d51fdb949499be67
TheRealRondon <amirhossein2324236@gmail.com>
2022-11-30 23:05:28 +0330

    Add project files.

---
+++ DapperAuthentication/Enitities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace DapperAuthentication.Enitities;
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string NormalizedUserName { get; set; }
+    public string Email { get; set; }
+    public string NormalizedEmail { get; set; }
+    public string PasswordHash { get; set; }
+}
