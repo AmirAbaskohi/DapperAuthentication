8103e728ef0ea8ed923b1f05da580dc04ace9614
TheRealRondon <amirhossein2324236@gmail.com>
2022-12-01 00:26:11 +0330

    Added signup.

--- DapperAuthentication/Controllers/UserController.cs
+++ DapperAuthentication/Controllers/UserController.cs
@@ -17,12 +17,31 @@ namespace DapperAuthentication.Controllers
             _config = config;
         }
 
-        [HttpGet]
+        [HttpPost, Route("signUp")]
+        public async Task<IActionResult> CreateUser(UserModel userModel)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var newUser = new User
+            {
+                Email = userModel.Email,
+                UserName = userModel.UserName,
+                NormalizedEmail = userModel.Email.ToUpper(),
+                NormalizedUserName = userModel.UserName.ToUpper(),
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(userModel.Password)
+            };
+            await connection.ExecuteAsync(@"INSERT INTO [User]
+                                            (UserName, NormalizedUserName, Email, NormalizedEmail, PasswordHash)
+                                            VALUES
+                                            (@UserName, @NormalizedUserName, @Email, @NormalizedEmail, @PasswordHash)", newUser);
+            return Ok();
+        }
+
+        [HttpGet, Route("getUsers")]
         public async Task<ActionResult<List<UserModel>>> GetAllUsers()
         {
             using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             var users = await connection.QueryAsync<User>("SELECT * FROM [User]");
-            var usermodels = users.Select(user => new UserModel {
+            var usermodels = users.Select(user => new {
                 UserName = user.UserName,
                 Email = user.Email
             });
--- DapperAuthentication/Models/UserModel.cs
+++ DapperAuthentication/Models/UserModel.cs
@@ -2,5 +2,6 @@
 public class UserModel
 {
     public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
 }
