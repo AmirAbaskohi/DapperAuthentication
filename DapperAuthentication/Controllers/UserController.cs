f6ff40bb2c356c32456288afc62f8085b029c2e2
TheRealRondon <amirhossein2324236@gmail.com>
2022-11-30 23:36:18 +0330

    Addet get users api.

--- DapperAuthentication/Controllers/UserController.cs
+++ DapperAuthentication/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DapperAuthentication.Models;
+using DapperAuthentication.Enitities;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace DapperAuthentication.Controllers
 {
@@ -7,5 +10,23 @@ namespace DapperAuthentication.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public UserController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserModel>>> GetAllUsers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var users = await connection.QueryAsync<User>("SELECT * FROM [User]");
+            var usermodels = users.Select(user => new UserModel {
+                UserName = user.UserName,
+                Email = user.Email
+            });
+            return Ok(usermodels);
+        }
     }
 }
