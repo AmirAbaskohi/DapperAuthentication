9cd5c77ef80aac8aa8ca674c1dc39203c6ffad8e
TheRealRondon <amirhossein2324236@gmail.com>
2022-12-01 23:46:32 +0330

    Added sqls for setting the database up.

---
+++ DapperAuthentication/Sql/UserTableCreation.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [UserName] NVARCHAR(256) NOT NULL,
+    [NormalizedUserName] NVARCHAR(256) NOT NULL,
+    [Email] NVARCHAR(256) NULL,
+    [NormalizedEmail] NVARCHAR(256) NULL,
+    [PasswordHash] NVARCHAR(MAX) NULL
+)
+ 
+GO
+ 
+CREATE INDEX [IX_User_NormalizedUserName] ON [dbo].[User] ([NormalizedUserName])
+ 
+GO
+ 
+CREATE INDEX [IX_User_NormalizedEmail] ON [dbo].[User] ([NormalizedEmail])
